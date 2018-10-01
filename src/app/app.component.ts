fbafe990bb25bfc7965b640b297284a2341dc04d
Preston Lamb <preston.j.lamb@gmail.com>
2018-10-01 14:05:31 -0600

    added a basic demo in the main app.module for the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RuntimeConfigLoaderService } from 'runtime-config-loader-lib';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'runtime-config-loader';
+export class AppComponent implements OnInit {
+	title = 'runtime-config-loader';
+
+	constructor(private _configSvc: RuntimeConfigLoaderService) {}
+
+	ngOnInit() {
+		const config = this._configSvc.getConfig();
+		console.log('config: ', config);
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
+
+import { initConfig, RuntimeConfigLoaderModule, RuntimeConfigLoaderService } from 'runtime-config-loader-lib';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent],
+	imports: [BrowserModule, RuntimeConfigLoaderModule],
+	providers: [
+		{
+			provide: APP_INITIALIZER,
+			useFactory: initConfig,
+			deps: [RuntimeConfigLoaderService],
+			multi: true,
+		},
+	],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
