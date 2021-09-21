51ac811d6bfcb3db2dd98385a75b4b129a6fc94e
Preston Lamb <preston.j.lamb@gmail.com>
2021-09-21 14:36:58 -0600

    added the module to the demo app to make sure it was loaded; updated the package's package.json

--- apps/runtime-config-loader-demo/src/app/app.module.ts
+++ apps/runtime-config-loader-demo/src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RuntimeConfigLoaderModule } from 'runtime-config-loader';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
-  providers: [],
-  bootstrap: [AppComponent],
+	declarations: [AppComponent],
+	imports: [
+		BrowserModule,
+		RuntimeConfigLoaderModule.forRoot({
+			configUrl: './assets/config/config.json',
+		}),
+	],
+	providers: [],
+	bootstrap: [AppComponent],
 })
 export class AppModule {}
