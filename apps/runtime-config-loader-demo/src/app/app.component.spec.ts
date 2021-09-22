ffb1ce7b25d7f1e5e7fecd1d633ca5758b839b7f
Preston Lamb <preston.j.lamb@gmail.com>
2021-09-21 21:38:28 -0600

    test: fix demo app test and create affected test command

--- apps/runtime-config-loader-demo/src/app/app.component.spec.ts
+++ apps/runtime-config-loader-demo/src/app/app.component.spec.ts
@@ -1,31 +1,17 @@
-import { TestBed } from '@angular/core/testing';
+import { RuntimeConfigLoaderService } from 'runtime-config-loader';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [AppComponent],
-    }).compileComponents();
-  });
+	let app: AppComponent;
+	let mockRuntimeConfigService = {} as RuntimeConfigLoaderService;
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+	beforeEach(() => {
+		app = new AppComponent(
+			mockRuntimeConfigService as RuntimeConfigLoaderService
+		);
+	});
 
-  it(`should have as title 'runtime-config-loader-demo'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('runtime-config-loader-demo');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome to runtime-config-loader-demo!'
-    );
-  });
+	it('should create the app', () => {
+		expect(app).toBeTruthy();
+	});
 });
