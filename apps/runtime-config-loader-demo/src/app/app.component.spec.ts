85bc172a8bcf99b80426320c8bf479acc65296cc
Preston Lamb <preston.j.lamb@gmail.com>
2026-02-05 23:18:19 -0700

    chore: update to nx 22

--- apps/runtime-config-loader-demo/src/app/app.component.spec.ts
+++ apps/runtime-config-loader-demo/src/app/app.component.spec.ts
@@ -1,16 +1,13 @@
 import { RuntimeConfigLoaderService } from 'runtime-config-loader';
 import { AppComponent } from './app.component';
-
 describe('AppComponent', () => {
 	let app: AppComponent;
 	const mockRuntimeConfigService = {} as RuntimeConfigLoaderService;
-
 	beforeEach(() => {
 		app = new AppComponent(
 			mockRuntimeConfigService as RuntimeConfigLoaderService
 		);
 	});
-
 	it('should create the app', () => {
 		expect(app).toBeTruthy();
 	});
--- libs/runtime-config-loader/src/lib/runtime-config-loader/runtime-config-loader.service.spec.ts
+++ libs/runtime-config-loader/src/lib/runtime-config-loader/runtime-config-loader.service.spec.ts
@@ -3,7 +3,6 @@ import { waitForAsync } from '@angular/core/testing';
 import { of, throwError } from 'rxjs';
 import { RuntimeConfig } from '../runtime-config';
 import { RuntimeConfigLoaderService } from './runtime-config-loader.service';
-
 describe('RuntimeConfigLoaderService', () => {
 	let service: RuntimeConfigLoaderService;
 	const mockSingleConfig: RuntimeConfig = {
@@ -26,7 +25,6 @@ describe('RuntimeConfigLoaderService', () => {
 		},
 	};
 	let mockHttpService: any;
-
 	describe('Single Config URL', () => {
 		beforeEach(() => {
 			mockHttpService = { get: null } as any as HttpClient;
@@ -35,51 +33,34 @@ describe('RuntimeConfigLoaderService', () => {
 				mockSingleConfig
 			);
 		});
-
 		it('should be created', () => {
 			expect(service).toBeTruthy();
 		});
-
-		it(
-			'should load the config object',
-			waitForAsync(() => {
-				mockHttpService.get = jest
-					.fn()
-					.mockReturnValue(of(mockConfigData1));
-				service.loadConfig().subscribe();
-
-				const config = service.getConfig();
-				expect(config).toStrictEqual(mockConfigData1);
-			})
-		);
-
-		it(
-			'should return the key from the config object',
-			waitForAsync(() => {
-				mockHttpService.get = jest
-					.fn()
-					.mockReturnValue(of(mockConfigData1));
-				service.loadConfig().subscribe();
-
-				const configKey = service.getConfigObjectKey('apiUrl');
-				expect(configKey).toStrictEqual(mockConfigData1.apiUrl);
-			})
-		);
-
-		it(
-			'should handle a load error',
-			waitForAsync(() => {
-				mockHttpService.get = jest
-					.fn()
-					.mockReturnValue(throwError('Test Error'));
-				service.loadConfig().subscribe();
-
-				const config = service.getConfig();
-				expect(config).toStrictEqual(null);
-			})
-		);
+		it('should load the config object', waitForAsync(() => {
+			mockHttpService.get = jest
+				.fn()
+				.mockReturnValue(of(mockConfigData1));
+			service.loadConfig().subscribe();
+			const config = service.getConfig();
+			expect(config).toStrictEqual(mockConfigData1);
+		}));
+		it('should return the key from the config object', waitForAsync(() => {
+			mockHttpService.get = jest
+				.fn()
+				.mockReturnValue(of(mockConfigData1));
+			service.loadConfig().subscribe();
+			const configKey = service.getConfigObjectKey('apiUrl');
+			expect(configKey).toStrictEqual(mockConfigData1.apiUrl);
+		}));
+		it('should handle a load error', waitForAsync(() => {
+			mockHttpService.get = jest
+				.fn()
+				.mockReturnValue(throwError('Test Error'));
+			service.loadConfig().subscribe();
+			const config = service.getConfig();
+			expect(config).toStrictEqual(null);
+		}));
 	});
-
 	describe('Double Config URL', () => {
 		beforeEach(() => {
 			mockHttpService = { get: null } as any as HttpClient;
@@ -88,65 +69,44 @@ describe('RuntimeConfigLoaderService', () => {
 				mockDoubleConfig
 			);
 		});
-
 		it('should be created', () => {
 			expect(service).toBeTruthy();
 		});
-
-		it(
-			'should load the config object from both sources',
-			waitForAsync(() => {
-				mockHttpService.get = jest
-					.fn()
-					.mockReturnValueOnce(of(mockConfigData1))
-					.mockReturnValueOnce(of(mockConfigData2));
-				service.loadConfig().subscribe();
-
-				const config = service.getConfig();
-				expect(config).toStrictEqual(mockConfigData2);
-			})
-		);
-
-		it(
-			'should return the key from the config object after combining both sources',
-			waitForAsync(() => {
-				mockHttpService.get = jest
-					.fn()
-					.mockReturnValueOnce(of(mockConfigData1))
-					.mockReturnValueOnce(of(mockConfigData2));
-				service.loadConfig().subscribe();
-
-				const configKey = service.getConfigObjectKey('apiUrl');
-				expect(configKey).toStrictEqual(mockConfigData2.apiUrl);
-			})
-		);
-
-		it(
-			'should handle a load error when the first file fails',
-			waitForAsync(() => {
-				mockHttpService.get = jest
-					.fn()
-					.mockReturnValueOnce(throwError('Test Error'))
-					.mockReturnValueOnce(of(mockConfigData2));
-				service.loadConfig().subscribe();
-
-				const config = service.getConfig();
-				expect(config).toStrictEqual(null);
-			})
-		);
-
-		it(
-			'should handle a load error when the second file fails',
-			waitForAsync(() => {
-				mockHttpService.get = jest
-					.fn()
-					.mockReturnValueOnce(of(mockConfigData1))
-					.mockReturnValueOnce(throwError('Test Error'));
-				service.loadConfig().subscribe();
-
-				const config = service.getConfig();
-				expect(config).toStrictEqual(null);
-			})
-		);
+		it('should load the config object from both sources', waitForAsync(() => {
+			mockHttpService.get = jest
+				.fn()
+				.mockReturnValueOnce(of(mockConfigData1))
+				.mockReturnValueOnce(of(mockConfigData2));
+			service.loadConfig().subscribe();
+			const config = service.getConfig();
+			expect(config).toStrictEqual(mockConfigData2);
+		}));
+		it('should return the key from the config object after combining both sources', waitForAsync(() => {
+			mockHttpService.get = jest
+				.fn()
+				.mockReturnValueOnce(of(mockConfigData1))
+				.mockReturnValueOnce(of(mockConfigData2));
+			service.loadConfig().subscribe();
+			const configKey = service.getConfigObjectKey('apiUrl');
+			expect(configKey).toStrictEqual(mockConfigData2.apiUrl);
+		}));
+		it('should handle a load error when the first file fails', waitForAsync(() => {
+			mockHttpService.get = jest
+				.fn()
+				.mockReturnValueOnce(throwError('Test Error'))
+				.mockReturnValueOnce(of(mockConfigData2));
+			service.loadConfig().subscribe();
+			const config = service.getConfig();
+			expect(config).toStrictEqual(null);
+		}));
+		it('should handle a load error when the second file fails', waitForAsync(() => {
+			mockHttpService.get = jest
+				.fn()
+				.mockReturnValueOnce(of(mockConfigData1))
+				.mockReturnValueOnce(throwError('Test Error'));
+			service.loadConfig().subscribe();
+			const config = service.getConfig();
+			expect(config).toStrictEqual(null);
+		}));
 	});
 });
